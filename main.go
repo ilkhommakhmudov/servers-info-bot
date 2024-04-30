f4429ead99fcb49fdfef186dbc1f681dbace6f42
Ilkhom Makhmudov <makhmudovilkhom@gmail.com>
2024-04-30 15:19:50 +0500

    - added multiple service notifier map

--- main.go
+++ main.go
@@ -12,12 +12,10 @@ import (
 
 var bot *tgbotapi.BotAPI
 var token string
-var chatID int64 = -1001864417452
-var services = map[string]string{
-	"app.hippo.uz":       "https://app.hippo.uz/ping",
-	"excel.app.hippo.uz": "https://app.hippo.uz/excel/ping",
-	"hippo.sog.uz":       "https://hippo.sog.uz/api/ping",
-}
+var administrationChatID int64 = -1001864417452 // admin tasks
+var supportChatID int64 = -1002031508824
+
+var notifierMap map[int64][]Service
 
 func handleUpdate(update tgbotapi.Update) {
 	// do nothing
@@ -41,6 +39,22 @@ func init() {
 	}
 
 	token = os.Getenv("TELEGRAM_BOT_TOKEN")
+
+	notifierMap = make(map[int64][]Service)
+	notifierMap[administrationChatID] = []Service{
+		{
+			CheckUrl: "https://app.hippo.uz/ping",
+			Name:     "app.hippo.uz",
+		},
+		{
+			CheckUrl: "https://hippo.sog.uz/api/ping",
+			Name:     "hippo.sog.uz",
+		},
+	}
+	notifierMap[supportChatID] = []Service{{
+		CheckUrl: "https://app.hippo.uz/excel/ping",
+		Name:     "app.hippo.uz - excel service",
+	}}
 }
 
 func main() {
@@ -53,21 +67,27 @@ func main() {
 	go func() {
 		var client = &http.Client{Timeout: time.Second * 5}
 		for {
-			for serviceName, serviceUrl := range services {
-				var request, _ = http.NewRequest(http.MethodGet, serviceUrl, nil)
-				switch response, err := client.Do(request); {
-				case err != nil:
-					var message = fmt.Sprintf("%s: CONNECTION FAILED", serviceName)
-					bot.Send(tgbotapi.NewMessage(chatID, message))
+			for chatID, services := range notifierMap {
+				for _, service := range services {
+					var request, _ = http.NewRequest(http.MethodGet, service.CheckUrl, nil)
+					switch response, err := client.Do(request); {
+					case err != nil:
+						var message = fmt.Sprintf("%s: CONNECTION FAILED", service.Name)
+						bot.Send(tgbotapi.NewMessage(chatID, message))
 
-				case response.StatusCode != http.StatusOK:
-					var message = fmt.Sprintf("%s: status code: %d", serviceName, response.StatusCode)
-					bot.Send(tgbotapi.NewMessage(chatID, message))
+					case response.StatusCode != http.StatusOK:
+						var message = fmt.Sprintf("%s: status code: %d", service.Name, response.StatusCode)
+						bot.Send(tgbotapi.NewMessage(chatID, message))
+					}
 				}
 			}
-
 			<-time.After(time.Second * 10)
 		}
 	}()
 	<-waitChan
+}
+
+type Service struct {
+	CheckUrl string
+	Name     string
 }
